b92c245a379aeebe081b05d2b352608d9c771c30
100amazingapps <134284073+100amazingapps@users.noreply.github.com>
2023-05-23 16:39:08 +0300

    Initial commit

---
+++ lib/constants/app.dart
@@ -0,0 +1,13 @@
+const String baseUrl = 'https://studyloan.forexnews.trade/paisa/';
+
+const String consumerKey = 'ck_20715b31e6ce426aea7b4fc7a4f3ab78eb798241';
+
+const String consumerSecret = 'cs_be51f96b3bed5f8a5471d1f8482ee39fd46b735e';
+
+const String restPrefix = 'wp-json';
+
+const String defaultLanguage = 'en';
+
+const List<String> languageSupport = ['en', 'ar', 'tr', 'id'];
+
+const String googleClientId = '101598264304-100lq3gaai2sa8qp3fh20v83atrgsf3r.apps.googleusercontent.com';
